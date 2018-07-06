85691f6961e1c204198a4dd53537ee665e54b135
WinmezzZ <winme0308@gmail.com>
2018-07-06 10:53:22 +0800

    加入babel-polyfill，兼容ie9

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -58,7 +58,7 @@ module.exports = {
   // You can exclude the *.map files from the build during deployment.
   devtool: false,
   // In production, we only want to load the polyfills and the app code.
-  entry: [require.resolve('./polyfills'), paths.appIndexJs],
+  entry: [require.resolve('./polyfills'), require.resolve('babel-polyfill'), paths.appIndexJs],
   output: {
     // The build folder.
     path: paths.appBuild,
