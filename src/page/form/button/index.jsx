028e991d1e8ad078f1da55f16e384b70534e8229
Winme <winme0308@gmail.com>
2018-07-07 13:24:25 +0800

    压缩优化

--- src/page/form/button/index.jsx
+++ src/page/form/button/index.jsx
@@ -7,30 +7,30 @@ import Icon from './icon';
 import './index.less';
 
 export default class App extends React.Component {
+  state = {
+    tabItems: ['a' ,'b' ,'c'],
+    index: 0,
+    width: 100
+  }
+  onclick = (e, index) => {
+    const width = parseInt(getComputedStyle(e.currentTarget).left) + 100
+    this.setState({
+      width,
+      index
+    })
+  }
   render() {
     return (
-      <Row gutter={10} className="button-page">
-        <Col md={12}>
-          <Card title="基本用法">
-            <Base/>
-          </Card>
-        </Col>
-        <Col md={12}>
-          <Card title="不同尺寸">
-            <Size/>
-          </Card>
-        </Col>
-        <Col md={12}>
-          <Card title="加载中">
-            <Loading/>
-          </Card>
-        </Col>
-        <Col md={12}>
-          <Card title="带图标">
-            <Icon/>
-          </Card>
-        </Col>
-      </Row>
+      <div>
+        <ul className="tabbar">
+            {
+              this.state.tabItems.map((item,index) => 
+                <li className={this.state.index === index ? 'this' : ''} key={index} onClick={(e) => this.onclick(e, index)}>{item}</li>
+              )
+            }
+          </ul>
+          <div className="underline" style={{width: this.state.width}}></div>
+      </div>
     )
   }
 }
