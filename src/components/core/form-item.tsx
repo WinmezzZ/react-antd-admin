e796cc475c58bf30c97c49b954929546728c8294
Winme <winme0308@gmail.com>
2021-05-27 18:42:08 +0800

    Comment added

--- src/components/core/form-item.tsx
+++ src/components/core/form-item.tsx
@@ -79,6 +79,8 @@ class GetControl implements GetBasicControlsProps, GetAdvancedControlsProps {
 // 使用 2 种类型来约束外部 props，当 type 属性为 advancedControls 中的一种时，需要提供 options 选项
 const MyFormItem: FC<BasicControlProps | AdvancedControlProps> = props => {
   const { children, type, ...rest } = props;
+  // Why do we need to use class: To optimize readability and maintainability, otherwise we have to use a lot of if and else to handle the logic of various types
+  // 为什么需要使用class：为了优化可读性和可维护性，否则只能使用大量的 if else 处理各种 type 的逻辑
   const control = type ? new GetControl()[type](props as never) : props.children;
 
   return <Form.Item {...rest}>{control}</Form.Item>;
