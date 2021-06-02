87c73c8c9c6aeb1c6d1a0d55e071896e5d9f6fba
Winme <winme0308@gmail.com>
2021-06-02 17:54:25 +0800

    extends form options property

--- src/components/core/form/index.tsx
+++ src/components/core/form/index.tsx
@@ -1,18 +1,35 @@
-import React, { FC } from 'react';
+import React from 'react';
 import { Form } from 'antd';
 import FormItem from '../form-item';
-import InternalForm from 'antd/lib/form/Form';
+import { FormProps } from 'antd/lib/form/Form';
+import { Controls, MyFormItemProps } from '../form-item/types';
+import MyFormItem from '../form-item';
 
-const MyForm: FC = props => {
-  return <Form {...props} />;
+export interface MyFormOptions extends Array<MyFormItemProps<Controls | undefined>> {}
+
+export interface MyFormProps<T> extends FormProps<T> {
+  options?: MyFormOptions;
+}
+
+const MyForm = <Values extends object>(props: MyFormProps<Values>) => {
+  const { options, children } = props;
+  return (
+    <Form<Values> {...props}>
+      {options
+        ? options.map(option => {
+            return <MyFormItem {...option} />;
+          })
+        : children}
+    </Form>
+  );
 };
 
 Object.assign(MyForm, Form, { Item: FormItem });
 
 type FormType = Omit<typeof Form, 'Item'>;
-type InternalFormType = typeof InternalForm;
+type SelfType = typeof MyForm;
 
-interface FormInterface extends FormType, InternalFormType {
+interface FormInterface extends FormType, SelfType {
   Item: typeof FormItem;
 }
 
