781a5bdca9f0ffef8c8463d5659beeb6cdbc7f79
Winme <winme0308@gmail.com>
2021-06-04 00:40:58 +0800

    add emotionjs

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,4 @@
-const { override, addLessLoader } = require('customize-cra');
+const { override, addLessLoader, addBabelPreset } = require('customize-cra');
 
 const rewiredSourceMap = () => config => {
   config.devtool = config.mode === 'development' ? 'cheap-module-source-map' : false;
@@ -6,6 +6,7 @@ const rewiredSourceMap = () => config => {
 };
 
 module.exports = override(
+  addBabelPreset('@emotion/babel-preset-css-prop'),
   addLessLoader({
     modifyVars: {
       '@primary-color': '#13c2c2'
