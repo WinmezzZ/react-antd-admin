c39467af6cb7b3fdafe57afcb49e7760c7815fad
Winme <winme0308@gmail.com>
2021-06-24 18:47:44 +0800

    extends page component ref methods

--- src/components/business/page/index.tsx
+++ src/components/business/page/index.tsx
@@ -2,107 +2,112 @@ import { css } from '@emotion/react';
 import { MyResponse } from 'api/request';
 import MyTable from 'components/core/table';
 import { PageData } from 'interface';
-import React, { useCallback, useEffect, useImperativeHandle, useState } from 'react';
+import React, { forwardRef, useCallback, useEffect, useImperativeHandle, useState } from 'react';
 import MyAside, { MyAsideProps } from '../aside';
 import MySearch from '../search';
+
 interface SearchApi {
   (params?: any): MyResponse<PageData<any>>;
 }
 
 type ParseDataType<S> = S extends (params?: any) => MyResponse<PageData<infer T>> ? T[] : any[];
 
-interface PageProps<S> {
+export interface PageProps<S> {
   searchRender?: React.ReactNode;
   pageApi?: S;
-  pageParams: Record<string, any>;
+  pageParams?: Record<string, any>;
   tableRender?: (data: ParseDataType<S>) => React.ReactNode;
   asideData?: any[];
   asideKey?: string;
   asideTreeItemRender?: MyAsideProps['titleRender'];
 }
 
-interface PageRef {
+export interface PageRef {
   setAsideCheckedKey: (key?: string) => void;
   load: (data?: object) => Promise<void>;
 }
 
-const MyPage = <S extends SearchApi>(props: React.PropsWithChildren<PageProps<S>>, ref: React.Ref<PageRef>) => {
-  const { pageApi, pageParams, searchRender, tableRender, asideKey, asideData, asideTreeItemRender } = props;
-  const [pageNum, setPageNum] = useState(0);
-  const [pageSize, setPageSize] = useState(20);
-  const [data, setData] = useState<ParseDataType<S>>([]);
-  const [asideCheckedKey, setAsideCheckedKey] = useState<string | number>();
+const BasePage = forwardRef(
+  <S extends SearchApi>(props: React.PropsWithChildren<PageProps<S>>, ref: React.Ref<PageRef>) => {
+    const { pageApi, pageParams, searchRender, tableRender, asideKey, asideData, asideTreeItemRender } = props;
+    const [pageNum, setPageNum] = useState(0);
+    const [pageSize, setPageSize] = useState(20);
+    const [data, setData] = useState<ParseDataType<S>>([]);
+    const [asideCheckedKey, setAsideCheckedKey] = useState<string | number>();
 
-  const getPageData = useCallback(
-    async (params: Record<string, any> = {}) => {
-      if (asideKey && !asideCheckedKey) return;
+    const getPageData = useCallback(
+      async (params: Record<string, any> = {}) => {
+        if (asideKey && !asideCheckedKey) return;
 
-      if (pageApi) {
-        const obj = {
-          ...params,
-          ...pageParams,
-          pageSize,
-          pageNum,
-          [asideKey!]: asideCheckedKey
-        };
-        const res = await pageApi(obj);
-        if (res.status) {
-          setPageNum(res.result.pageNum);
-          setPageSize(res.result.pageSize);
-          setData(res.result.data);
+        if (pageApi) {
+          const obj = {
+            ...params,
+            ...pageParams,
+            pageSize,
+            pageNum,
+            [asideKey!]: asideCheckedKey
+          };
+          const res = await pageApi(obj);
+          if (res.status) {
+            setPageNum(res.result.pageNum);
+            setPageSize(res.result.pageSize);
+            setData(res.result.data);
+          }
         }
-      }
-    },
-    [pageApi, pageParams, pageSize, pageNum, asideKey, asideCheckedKey]
-  );
+      },
+      [pageApi, pageParams, pageSize, pageNum, asideKey, asideCheckedKey]
+    );
 
-  useEffect(() => {
-    getPageData();
-  }, [getPageData]);
+    useEffect(() => {
+      getPageData();
+    }, [getPageData]);
 
-  const onSearch = (searchParams: Record<string, any>) => {
-    getPageData(searchParams);
-  };
+    const onSearch = (searchParams: Record<string, any>) => {
+      getPageData(searchParams);
+    };
 
-  const onSelectAsideTree: MyAsideProps['onSelect'] = ([key]) => {
-    setAsideCheckedKey(key);
-  };
+    const onSelectAsideTree: MyAsideProps['onSelect'] = ([key]) => {
+      setAsideCheckedKey(key);
+    };
 
-  useImperativeHandle(ref, () => ({
-    setAsideCheckedKey,
-    load: (data?: object) => getPageData(data)
-  }));
+    useImperativeHandle(ref, () => ({
+      setAsideCheckedKey,
+      load: (data?: object) => getPageData(data)
+    }));
 
-  return (
-    <div css={styles}>
-      {asideData && (
-        <MyAside
-          checkedKeys={asideCheckedKey ? [asideCheckedKey] : undefined}
-          titleRender={asideTreeItemRender}
-          onSelect={onSelectAsideTree}
-        />
-      )}
-      <div>
-        {searchRender && <MySearch onSearch={onSearch}>{searchRender}</MySearch>}
-        {tableRender && (
-          <MyTable dataSource={data} pagination={{ current: pageNum, pageSize: pageSize }}>
-            {tableRender(data)}
-          </MyTable>
+    return (
+      <div css={styles}>
+        {asideData && (
+          <MyAside
+            checkedKeys={asideCheckedKey ? [asideCheckedKey] : undefined}
+            titleRender={asideTreeItemRender}
+            onSelect={onSelectAsideTree}
+          />
         )}
+        <div>
+          {searchRender && <MySearch onSearch={onSearch}>{searchRender}</MySearch>}
+          {tableRender && (
+            <MyTable dataSource={data} pagination={{ current: pageNum, pageSize: pageSize }}>
+              {tableRender(data)}
+            </MyTable>
+          )}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+);
+
+BasePage.defaultProps = {
+  pageParams: {}
 };
 
-MyPage.MySearch = MySearch;
-MyPage.MyTable = MyTable;
-MyPage.MyAside = MyAside;
+const MyPge = Object.assign(BasePage, {
+  MySearch,
+  MyTable,
+  MyAside
+});
 
-MyPage.defaultProps = {
-  pageParams: {}
-} as PageProps<any>;
-
-export default MyPage;
+export default MyPge;
 
 const styles = css`
   display: flex;
