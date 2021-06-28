9f172609ec6ce8806df796fd396c9d3bdfbfe8e1
Winme <winme0308@gmail.com>
2021-06-28 11:31:03 +0800

    chore: add useStates hooks

---
+++ src/utils/use-states.ts
@@ -0,0 +1,7 @@
+import { useState } from 'react';
+
+export function useStates<T extends object>(initialState: T): [T, (state: Partial<T>) => void] {
+  const [state, setState] = useState(initialState);
+  const setMergedState = (newState: Partial<T>) => setState(prevState => Object.assign({}, prevState, newState));
+  return [state, setMergedState];
+}
